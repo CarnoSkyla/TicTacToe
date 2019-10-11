659ba228d4049c122f7ed3651481fbe0a89f7f34
Sibusiso Labi <skylasbu@gmail.com>
2019-10-11 15:50:50 +0200

    Updated Self-Practice_Challenges

--- Self-Practice/TicTacToe.cs
+++ Self-Practice/TicTacToe.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TicTacToeRendererLib.Renderer;
 
 namespace Self_Practice
 {
     public class TicTacToe
     {
+       
+         
+            private TicTacToeConsoleRenderer _boardRenderer;
+
+            public TicTacToe()
+            {
+                _boardRenderer = new TicTacToeConsoleRenderer(10, 6);
+                _boardRenderer.Render();
+            }
+
+
         public static void TTT()
         {
-            char[] TicTactToeBoard = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-            char defaultValue = ' ';
 
+        
             //Board
 
-            
+
         }
     }
+
 }
+
